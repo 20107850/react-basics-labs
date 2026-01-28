ec052931bebcc86ef87d7bdedb8afa79f0a4c8a5
ice <20107850@mail.wit.ie>
2026-01-28 17:14:04 +0000

    Tasky Lab 2b - Exercises done

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -27,7 +27,8 @@ function App() {
   const [ formState, setFormState ] = useState({
     title: "",
     description: "",
-    deadline: ""
+    deadline: "",
+    priority: ""
   });
 
     const formSubmitHandler = (event) => {
@@ -54,6 +55,9 @@ function App() {
           break;
       case "deadline":
           form.deadline = event.target.value;
+          break;
+          case "priority":
+          form.priority = event.target.value;
           break;
       default:
           form = formState;
--- tasky/src/components/Form.jsx
+++ tasky/src/components/Form.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 
 const AddTaskForm = (props) => {
+let priorityClass = "";
+
+if (props.priority === "Low") {
+  priorityClass = "priority low";
+} else if (props.priority === "Medium") {
+  priorityClass = "priority medium";
+} else if (props.priority === "High") {
+  priorityClass = "priority high";
+}
 
   return (
     <div>
@@ -20,6 +29,13 @@ const AddTaskForm = (props) => {
         Details:
         <input type="text" name="description" onChange={(event) => props.change(event)} />
     </label>
+    <br />
+    <label>
+    Priority:
+        <input type="text" name="priority" onChange={(event) => props.change(event)} />
+    </label>
+    <br />
+
     <input type="submit" value="Submit" />
         </form>
     </div>
